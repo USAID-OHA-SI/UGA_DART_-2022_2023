ed2ea55a94b70c8c468f775410dcd928a5e73588
Nelly Maina <87029048+nmaina@users.noreply.github.com>
2022-10-21 15:28:55 -0400

    Added documentation

--- Scripts/Effect size calculation.R
+++ Scripts/Effect size calculation.R
@@ -1,3 +1,50 @@
+# PROJECT: 
+# PURPOSE: 
+# AUTHOR: Nelly Maina| SI
+# REF ID:   8ef973df
+# LICENSE: MIT
+# DATE: 2022-10-21
+# NOTES: Nelly Maina| SI
+
+# LOCALS & SETUP ============================================================================
+
+  # Libraries
+    oha
+    
+  # SI specific paths/functions  
+    load_secrets()
+    merdata <- file.path(glamr::si_path("path_msd"))
+    file_path <- return_latest(folderpath = merdata,
+      pattern = "Site_IM_FY20-22_20220318_v2_1_Zambia")
+      
+  # Grab metadata
+   msd_source <- source_info(file_path)
+   curr_pd <- source_info(file_path, return = "period")
+   pd <- source_info(file_path, return = "period")
+   fy <- source_info(file_path, return = "fiscal_year")
+   qtr <- source_info(file_path, return = "quarter")  
+  
+  # REF ID for plots
+    ref_id <- "8ef973df"
+    
+  # Functions  
+  
+
+# LOAD DATA ============================================================================  
+
+  msd
+
+# MUNGE ============================================================================
+  
+  #  
+  
+# VIZ ============================================================================
+
+  #  
+
+# SPINDOWN ============================================================================
+
+
 #test github pull and push
 
 library(readxl)
